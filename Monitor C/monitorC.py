86c450f0e356f4e685d1557ecbfd57c0a51e2334
pmayavi <pmayav@eafit.edu.co>
2023-05-16 23:03:20 -0500

    Arreglo de MonitorC

--- Monitor C/monitorC.py
+++ Monitor C/monitorC.py
@@ -1,15 +1,27 @@
 # importar los requerimientos del gRPC para conectar los monitores
 from datetime import datetime
-import time
-import MicroService
-import socket
+from concurrent import futures
+import time, socket, grpc, Service_pb2, Service_pb2_grpc
 
-# Conexion por gRPC
-mc = MicroService()
+HOST = "[::]:8080"
+
+
+class MicroService(Service_pb2_grpc.MicroServiceServicer):
+    def CheckOnline(self, response, context):
+        return Service_pb2.Response(status=1)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    Service_pb2_grpc.add_MicroServiceServicer_to_server(MicroService(), server)
+    server.add_insecure_port(HOST)
+    print("Service is running... ")
+    server.start()
+    server.wait_for_termination()
 
 
 def tiempoEjecucion(self):
-    host="[::]:8080"
+    host = "[::]:8080"
     address, port = host.split("]:")
     address = address.strip("[]")
     ipv4_address = socket.getaddrinfo(address, port, socket.AF_INET)[0][4][0]
@@ -19,3 +31,6 @@ def tiempoEjecucion(self):
         mc(ipv4_address, processCap)
         processCap = (time.time - startTime) / 2.5
 
+
+if __name__ == "__main__":
+    serve()
